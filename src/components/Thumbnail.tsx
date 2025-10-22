be59474ea7131fb1bccd4cd6dd309e7d9d03dd7a
Rainer Mager <rainer@qythyx.com>
2025-10-22 21:15:18 +0900

    make Thumbnail more flexible

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,35 +1,68 @@
 import * as Icons from '@mui/icons-material';
-import React, { useCallback, useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, SxProps, Theme } from '@mui/material';
+import React, { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 import { ImageModal } from './ImageModal';
 
 interface ThumbnailProps {
 	alt?: string;
 	imageUrl?: string;
+	noImageIcon?: ReactNode | undefined;
+	onImageClick?: ((imageUrl: string, event: React.MouseEvent<HTMLElement, MouseEvent>) => void) | undefined | null;
+	onNoImageClick?: React.MouseEventHandler<HTMLElement> | undefined;
+	sx: SxProps<Theme> | undefined;
 }
 
 export interface ThumbnailRef {
 	openImage: (imageUrl: string) => void;
 }
 
-export function Thumbnail({ alt = 'Thumbnail', imageUrl }: ThumbnailProps): React.ReactElement {
-	const [imageModalState, setImageModalState] = useState<{
-		imageUrl: string;
-		isOpen: boolean;
-	}>({
+export function Thumbnail({
+	alt = 'Thumbnail',
+	imageUrl,
+	noImageIcon,
+	onImageClick,
+	onNoImageClick,
+	sx,
+}: ThumbnailProps): React.ReactElement {
+	const ref = useRef<HTMLDivElement>(null);
+	const [imageHeight, setImageHeight] = useState<number>(1);
+	const [imageModalState, setImageModalState] = useState<{ imageUrl: string; isOpen: boolean }>({
 		imageUrl: '',
 		isOpen: false,
 	});
 
-	const handleImageClick = useCallback((url: string, e: React.MouseEvent): void => {
-		e.stopPropagation();
-		if (url) {
-			setImageModalState({
-				imageUrl: url,
-				isOpen: true,
-			});
+	useEffect(() => {
+		const updateImageHeight = (): void => {
+			if (ref.current) {
+				const sibling = ref.current.parentElement;
+				if (sibling) {
+					const { height } = sibling.getBoundingClientRect();
+					setImageHeight(height);
+				}
+			}
+		};
+
+		updateImageHeight();
+		const resizeObserver = new ResizeObserver(updateImageHeight);
+		if (ref.current) {
+			resizeObserver.observe(ref.current);
 		}
+		return (): void => resizeObserver.disconnect();
 	}, []);
+
+	const handleImageClick =
+		onImageClick === null
+			? null
+			: (onImageClick ??
+				useCallback((url: string, e: React.MouseEvent): void => {
+					e.stopPropagation();
+					if (url) {
+						setImageModalState({
+							imageUrl: url,
+							isOpen: true,
+						});
+					}
+				}, []));
 
 	const handleCloseImageModal = useCallback((): void => {
 		setImageModalState({
@@ -41,34 +74,39 @@ export function Thumbnail({ alt = 'Thumbnail', imageUrl }: ThumbnailProps): Reac
 	return (
 		<>
 			<Box
+				ref={ref}
 				sx={{
 					alignContent: 'center',
 					alignItems: 'center',
 					border: imageUrl ? 'none' : 'solid 2px red',
 					borderRadius: '5px',
 					display: 'flex',
+					height: `${imageHeight}px`,
 					justifyContent: 'center',
-					maxWidth: '2em !important',
-					minWidth: '2em !important',
+					maxHeight: `${imageHeight}px`,
 					overflow: 'hidden',
+					width: '2em',
+					...sx,
 				}}
 			>
 				{imageUrl ? (
 					<img
 						alt={alt}
-						onClick={e => handleImageClick(imageUrl, e)}
+						onClick={e => handleImageClick?.(imageUrl, e)}
 						src={imageUrl}
 						style={{
 							alignSelf: 'center',
-							cursor: 'pointer',
-							maxHeight: '100%',
+							cursor: handleImageClick === null ? 'unset' : 'pointer',
 							maxWidth: '100%',
+							objectFit: 'cover',
 						}}
 					/>
 				) : (
 					<Box
+						onClick={e => onNoImageClick?.(e)}
 						sx={{
 							alignItems: 'center',
+							cursor: onNoImageClick ? 'pointer' : 'unset',
 							display: 'flex',
 							height: '100%',
 							justifyContent: 'center',
@@ -76,7 +114,7 @@ export function Thumbnail({ alt = 'Thumbnail', imageUrl }: ThumbnailProps): Reac
 							width: '100%',
 						}}
 					>
-						<Icons.AddPhotoAlternate />
+						{noImageIcon ?? <Icons.AddPhotoAlternate />}
 					</Box>
 				)}
 			</Box>
